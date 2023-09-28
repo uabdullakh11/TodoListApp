692d4bd59b9e59da43fe311defba882f3bb0f877
uabdullakh11 <uabdullakh11@gmail.com>
2023-09-28 14:03:19 +0300

    fix auth problems

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,18 +1,20 @@
-import User  from "../models/User.js";
+import User from "../models/User.js";
 import { comparePasswords, generateAccessToken } from "../utils/auth.js";
+import { userController } from "./index.js";
 
 const login = async (req, res) => {
+  console.log(req.body);
   if (!req.body) return res.sendStatus(400);
   const [login, password] = [req.body.login, req.body.password];
-  const user = await User.findOneOrFail({ login });
+  const user = await User.findOne({ where: {login} });
   if (!(await comparePasswords(password, user.password))) {
     console.log("Invalid login or password");
     res.send("Invalid login or password");
   }
   res.send(generateAccessToken(user.id))
-  return {
-    access: generateAccessToken(user.id),
-  };
+  // return {
+  //   access: generateAccessToken(user.id),
+  // };
 }
 
 const register = async (req, res) => {
@@ -24,7 +26,8 @@ const register = async (req, res) => {
   ];
   let user = await User.findOne({ where: { email } });
   if (user) console.log("User already exist");
-  user = await createUser(login, email, password);
+  user = await userController.createUser(login, email, password);
+  res.send(user)
   return user;
 }
 
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -3,7 +3,7 @@ import {authController} from '../controllers/index.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/login', authController.login);
-authRouter.post('/register', authController.register);
+authRouter.post('/login', authController.login)
+.post('/register', authController.register);
 
 export {authRouter}
